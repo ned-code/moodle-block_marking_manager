5669a5e6e1697e724e3474b1f543fb557512b456
Michael Gardener <mgardener@cissq.com>
2016-11-04 10:22:20 -0400

    Version update

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  * @developer  Michael Gardener <mgardener@cissq.com>
  */
 
-$plugin->version  = 2016103100;
+$plugin->version  = 2016110400;
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'block_fn_marking';
