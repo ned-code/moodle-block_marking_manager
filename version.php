6df2638fa669cad719814b79f164f73d638fa161
NED <fernandooliveira@knet.ca>
2017-01-10 16:21:16 -0500

    Update version.php

--- version.php
+++ version.php
@@ -27,4 +27,4 @@ $plugin->version  = 2017011000;
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'block_fn_marking';
-$plugin->release = '3.1.6';
+$plugin->release = '3.1.7';
