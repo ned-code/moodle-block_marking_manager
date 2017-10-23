c030b779b2f019af8992e3b48b26e8887c656460
Michael Gardener <mgardener@cissq.com>
2017-10-22 22:33:48 -0400

    Fix for grade difference between the Progress report and the gradebook

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  * @developer  Michael Gardener <mgardener@cissq.com>
  */
 
-$plugin->version  = 2017101700;
+$plugin->version  = 2017102200;
 $plugin->requires = 2015111600; // Moodle 3.0.x, 3.1.x or 3.2.x is required.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'block_fn_marking';
